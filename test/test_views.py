0a01716db7fc2ecb09bf3ddaab528ed7050ff69e
mbartczak1 <bartczak-maciej@wp.pl>
2025-04-25 21:11:10 +0200

    poprawki

--- test/test_views.py
+++ test/test_views.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import Flask
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
@@ -11,7 +12,7 @@ class FlaskrTestCase(unittest.TestCase):
     def test_outputs(self):
         rv = self.app.get('/outputs')
         s = str(rv.data)
-        ','.join(SUPPORTED) in s
+        self.assertIn(','.join(SUPPORTED), s)
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
